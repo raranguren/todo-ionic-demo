03d45b30b20a0b109091cab2dc85f9b5291b7708
raranguren <ricaragas@hotmail.com>
2023-04-26 10:53:46 +0200

    add buttons to add/delete tasks

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,13 +10,24 @@ import { TodoItem } from '../model/todo-item.model';
 export class HomePage {
 
   public todoList : TodoItem[];
+  public todoService : TodoService;
 
   constructor(todoService: TodoService) {
+    this.todoService = todoService;
     this.todoList = todoService.todoList;
   }
 
   public edit(index: number) {
     //TODO pop-up to edit
+  }
+
+  public add() {
+    //TODO pop-up to create new
+  }
+
+  public deleteAll() {
+    //TODO confirmation pop-up
+    this.todoService.deleteAll();
   }
 
 }
