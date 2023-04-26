27ea12641b3afcbaeec65c1d72b82e50ebb0a983
raranguren <ricaragas@hotmail.com>
2023-04-26 10:06:23 +0200

    service methods

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { TodoItem } from '../model/todo-item.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  // temporarily hardcoded
+  private todoList : TodoItem[] = [
+    new TodoItem("Comprar verduras del mercado"),
+    new TodoItem("Lavar la furgoneta"),
+  ];
+
+  constructor() { }
+
+  public deleteAll() {
+    this.todoList = [];
+  }
+
+  public add(description: string) {
+    this.todoList.unshift(new TodoItem(description));
+  }
+
+  public update(id: number, description: string) {
+    if (id >= this.todoList.length) return;
+    this.todoList[id].description = description;
+  }
+
+}
