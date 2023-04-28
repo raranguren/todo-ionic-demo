04e38acff225d5a9d5ef3c456117aceca1bc0885
raranguren.bts <ricardo.aranguren@beyondthesample.com>
2023-04-28 17:17:30 +0200

    Changes how API Key is loaded from ./environments

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { config } from 'dotenv';
 
 if (environment.production) {
   enableProdMode();
 }
-
-// Load environment variables from .env file
-config();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
